bae79222d02b54c420ac36aa4e7fdbc28c8db325
sudhircyanous <56716846+sudhircyanous@users.noreply.github.com>
2025-04-21 14:07:48 +0530

    Update LT.tf

--- LT.tf
+++ LT.tf
@@ -1,8 +1,8 @@
 resource "aws_launch_template" "project_lt" {
   name_prefix   = "project-lt-"
-  image_id      = "ami-0e449927258d45bc4"  # Amazon Linux 2 (us-east-1)
+  image_id      = "ami-0c02fb55956c7d316"  # Amazon Linux 2 (us-east-1)
   instance_type = "t2.micro"
-  key_name      = "project-key"
+  key_name      = "TF"  # Using your existing key pair named 'TF'
 
   vpc_security_group_ids = [aws_security_group.alb_sg.id]
 
